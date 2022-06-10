77da9babecf2975e06a705a7c38e771cd21b1ed3
g-make-it <g.make.it.channel@gmail.com>
2022-06-10 13:26:12 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+This is an Instagram automation project for web browsers.
+
+- Download / Clone
+- create a Virtual environment
+- install the dependencies in "requirements.txt"
+- Run
+
+## Check the video below for more details:
+
+[![Watch the video](https://img.youtube.com/vi/ZoVHNS1PABI/0.jpg)](https://www.youtube.com/watch?v=ZoVHNS1PABI)
+
+
+## [Subscribe](https://www.youtube.com/channel/UCsQqV_wq5yPrw5YIpvwmjvQ) to my youtube channel to find similar content.
---
+++ i_follow_you_follow.py
@@ -0,0 +1,32 @@
+# follow people and follow me back
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import random
+import time
+
+def sleep_for_period_of_time():
+    limit = random.randint(60,90)
+    time.sleep(limit)
+
+def main():
+    driver = webdriver.Chrome(executable_path=ChromeDriverManager.install())
+    driver.get("https://www.instagram.com")
+    print('stop')
+
+    while(True):
+        try:
+            list_of_people = driver.find_elements(By.XPATH, '//div[@class="_aaes"]//*//*')
+            for person in list_of_people:
+                if person.text == 'Follow':
+                    person.click()
+                    sleep_for_period_of_time()
+
+            driver.execute_script("arguments[0].scrollIntoView(true);", list_of_people[-1])
+
+        except Exception as e:
+            print(e)
+
+if __name__ == "__main__":
+    main()
+
